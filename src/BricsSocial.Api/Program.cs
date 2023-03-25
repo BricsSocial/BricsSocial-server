a19ad3888dccc0d962d05bbf7f61fbce701120c5
vbelovitsky <vbelovitsky@yandex.ru>
2023-03-25 17:07:41 +0300

    fix: add Position to agent create method

--- src/BricsSocial.Api/Program.cs
+++ src/BricsSocial.Api/Program.cs
@@ -36,7 +36,12 @@ using (var scope = app.Services.CreateScope())
     await initialiser.SeedAsync();
 }
 
-app.UseCors(builder => builder.AllowAnyOrigin());
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+});
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
